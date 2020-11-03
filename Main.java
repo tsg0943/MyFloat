adf28451e7191fb98e82ae44d081e4a55d8cddb7
tanya.grishina.02@gmail.com <tanya.grishina.02@gmail.com>
2020-11-03 22:24:29 +0300

    class Main

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            MyFloat a = new MyFloat(12343445, 6);
+            MyFloat b = new MyFloat(8656645, 4);
+            b.diff(a);
+    }
+}
+
